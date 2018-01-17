6eebea88d75006aea920bad34bfb1e139f09e282
Richard Hai <hirichardhai@gmail.com>
2018-01-17 12:14:56 -0800

    Completed push, pop, and size for stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,24 @@
 var Stack = function() {
   var someInstance = {};
 
+
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[Object.keys(storage).length] = value
+    return storage;
   };
 
   someInstance.pop = function() {
+    var tempStorage = storage[Object.keys(storage).length - 1];
+    delete storage[Object.keys(storage).length - 1]
+    return tempStorage;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
