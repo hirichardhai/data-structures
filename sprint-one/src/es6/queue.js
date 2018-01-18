bbd2841c5bdf8c4a074557e0aa9c2f4738efd53a
Richard Hai <hirichardhai@gmail.com>
2018-01-17 23:27:47 -0800

    Finished all es6 requirements.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,26 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.storage = {};
   }
 
+  enqueue(value) {
+  	var size = Object.keys(this.storage).length;
+    this.storage[size] = value
+  }
+  dequeue() {
+  	var size = Object.keys(this.storage).length
+    var tempValue = this.storage[0];
+    delete this.storage[0];
+    for (var i = 0; i < size; i++) {
+      this.storage[i] = this.storage[i + 1]; 
+      if (i === size - 1) {
+        delete this.storage[i];
+      }
+    }
+    return tempValue;
+  }
+  size() {
+	return Object.keys(this.storage).length;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,20 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.storage = {};
+  }
+  push(value) {
+  	var size = Object.keys(this.storage).length;
+  	this.storage[size] = value;
+  }
+  pop() {
+  	var size = Object.keys(this.storage).length;
+	var tempValue = this.storage[size - 1];
+	delete this.storage[size - 1];
+	return tempValue;
+  }
+  size() {
+  	return Object.keys(this.storage).length;
   }
 
 }
