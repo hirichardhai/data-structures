1bab1837bd32b9395d62741a4dee6d4173253388
Richard Hai <hirichardhai@gmail.com>
2018-01-17 16:19:19 -0800

    Finished stack.js for shared methods

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  extend(obj, stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	push: function(value) {
+		this[Object.keys(this).length - 3] = value;
+	},
+	pop: function() {
+		var tempStorage = this[Object.keys(this).length - 4];
+	    delete this[Object.keys(this).length - 4]
+	    return tempStorage;
+	},
+	size: function() {
+		return Object.keys(this).length - 3;
+	}
+};
 
-
+var extend = function(obj, methods) {
+    for (var key in methods) {
+    	obj[key] = methods[key];
+    }
+};
