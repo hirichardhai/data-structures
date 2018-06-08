9c88921efc04ba48f2c92fd2f0c64978f15da2c2
Richard Hai <hirichardhai@gmail.com>
2018-06-07 19:17:07 -0700

    semi finsiehd stack for functional

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,15 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    let size = someInstance.size();
+    storage[size] = value;
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
