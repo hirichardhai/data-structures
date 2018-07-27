d2f57aa9189302ef3f5bf9263993ce086ca0e911
Richard Hai <hirichardhai@gmail.com>
2018-07-26 18:31:31 -0700

    working solutions for queue shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,30 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  let storage = {};
-
   let someInstance = {};
 
-  function extend(obj1, obj2) {
-    for (var key in obj2) {
-      obj1[key] = obj2[key];
-    }
-    return obj1;
-  }
-  
-  return extend(someInstance, queueMethods);
+  someInstance['front'] = 0;
+  someInstance['count'] = 0;
 
+  extend(someInstance, queueMethods);
+  return someInstance;
 };
+
+function extend(obj1, obj2) {
+  for (var key in obj2) {
+    obj1[key] = obj2[key];
+  }
+}
 
 var queueMethods = {
   size: function() {
-    return Object.keys(this.storage).length;
+    return Object.keys(this).length - 5;
   },
   enqueue: function(value) {
-
+    this[this.count] = value;
+    this.count++;
   },
   dequeue: function() {
-
+    let returnValue = this[this.front];
+    delete this[this.front];
+    this.front++;
+    return returnValue;
   }
 };
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let instance = {};
+
+  extend(instance, stackMethods);
+
+  return instance;
 };
 
-var stackMethods = {};
+let extend = function(obj1, obj2) {
+  for (var key in obj2) {
+    obj1[key] = obj2[key];
+  }
+}
+
+var stackMethods = {
+  size: function() {
+    return Object.keys(this).length - 3;
+  },
+  push: function(value) {
+    let size = this.size();
+    this[size] = value;
+  },
+  pop: function() {
+    let value = this[this.size() - 1];
+    delete this[this.size() - 1];
+    return value;
+  }
+};
 
 
