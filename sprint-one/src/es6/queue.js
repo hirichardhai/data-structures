382a885ad53b73bba23ac6d120ed7829b582268d
Richard Hai <hirichardhai@gmail.com>
2018-07-26 19:32:44 -0700

    fully complete sprint 1 for data structures

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,25 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.storage = {
+      front: 0,
+      back: 0
+    };
   }
 
+  enqueue(value) {
+    this.storage[this.storage.back] = value;
+    this.storage.back++;
+  }
+
+  dequeue() {
+    let value = this.storage[this.storage.front];
+    delete this.storage[this.storage.front];
+    this.storage.front++;
+    return value;
+  }
+
+  size() {
+    return Object.keys(this.storage).length - 2;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,23 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {
+    }
   }
 
+  size() {
+    return Object.keys(this.storage).length;
+  }
+
+  pop() {
+    let size = this.size() - 1;
+    let value = this.storage[size];
+    delete this.storage[size];
+    return value;
+  }
+
+  push(value) {
+    let size = this.size();
+    this.storage[size] = value;
+  }
 }
